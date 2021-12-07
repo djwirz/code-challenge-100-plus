847a17986d820a423fc80d85b2a26c91f263451a
Dan Wirz <djwirz@gmail.com>
2021-12-07 07:00:01 -0700

    server setup and passing test strings

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+// parse request bodies (req.body)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// choose status code
+app.get("/generate", (req, res) => {
+  res.json({ data: "test" });
+});
+
+//   choose status code
+app.post("/validate", (req, res) => {
+  res.json({ data: "true" });
+});
+
+// assume 404 since no middleware responded
+app.use(function (req, res, next) {
+  res.status(404).render("404", { url: req.originalUrl });
+});
+
+app.listen(3000);
+console.log("Express started on port 3000");
